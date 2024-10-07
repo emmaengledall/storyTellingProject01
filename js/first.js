661355b10bc11e9630866d597eee8afb863ce803
Emma-Chantal Engledall <55669@dmjx.dk>
2024-10-07 14:15:06 +0200

    PlanetUnivers22

--- js/first.js
+++ js/first.js
@@ -1,6 +1,14 @@
 // VORES IMPORTS
 import * as THREE from 'three'; // Import af three.js 
 import {OrbitControls} from '/examples/jsm/controls/OrbitControls.js'; // Orbit control
+import { EffectComposer } from '/examples/jsm/postprocessing/EffectComposer.js'; // Så vi kan loade effetker ind
+import { RenderPass } from '/examples/jsm/postprocessing/RenderPass.js'; // Så vi kan rendere effetker
+
+// EFFEKT IMPORTS
+import {UnrealBloomPass} from 'three/addons/postprocessing/UnrealBloomPass.js'; // Vores bloom pass 
+
+
+
 
 // FUNDAMENTET - ALT BASALT SOM SKABER MINE OMGIVELSER
 var canvasEl = document.getElementById("three"); // vores canvas element 
@@ -21,6 +29,12 @@ renderer.setSize(vw, vh);
 renderer.setAnimationLoop(animate); 
 renderer.shadowMap.enabled = true; 
 renderer.shadowMap.type = true; 
+// lav Effektcomposer - så vi kan tilføje effekter
+const composer = new EffectComposer(renderer);
+// capture scene
+const renderScene = new RenderPass(scene, camera);
+composer.addPass(renderScene);
+
 
 
  // AMBIENTLIGHT
@@ -31,7 +45,6 @@ scene.add(ambiLight); // Tilføjer naturligt overall lys til omgivelserne
 
 // ANIMATIONS FUNKTION 
 function animate() {
-
     renderer.render(scene, camera); 
 }
 
@@ -42,7 +55,7 @@ function animate() {
 
 
 
-// THE GROUND 
+// GULVET - JORDEN
 const floorGeom = new THREE.PlaneGeometry(500, 500); // Geometrien af gulvet
 const floorTexture = new THREE.TextureLoader().load('examples/textures/terrain/grasslight-big.jpg') // Loader min texture ind 
 floorTexture.wrapS = THREE.RepeatWrapping; // Min texture gentager sig selv 
@@ -52,16 +65,18 @@ floorTexture.repeat.set(128,128); // Fortæller hvor stor min texture skal være
 const floorMat = new THREE.MeshStandardMaterial({color:0xffffff, map: floorTexture});
 const floor = new THREE.Mesh(floorGeom, floorMat); 
 floor.rotation.x = dtr(-90); // Hvilken vinkel vores floor skal ligge på
+floor.receiveShadow = true; 
 scene.add(floor); 
 
 
-// STARS 
+
+// STJERNER
 function createStars(){
     const points = [];
     const radius = 700; 
     for(let i=0;i<1000;i++){
         let angel = dtr(Math.random()*360);
-        let x = radius * Math.cos(angel);
+        let x = radius * Math.cos(angel) ;
         let z = radius * Math.sin(angel);
         let y = Math.random() * 365; 
         points.push(new THREE.Vector3(x,y,z)); 
@@ -73,19 +88,50 @@ function createStars(){
     return stars;
 };
 
-let _stars = new createStars();
+let stars = new createStars();
+
+
+
+
+
+// DE FORSKELLIGE PLANETTER 
+
+// KÆRLIGHEDENS PLANET
+let sphere;
+function buildTSphere(x, y, z) {
+    const geometry = new THREE.SphereGeometry(10, 32, 32); // Sphere geometrien
+    const sphereTexture = new THREE.TextureLoader().load('examples/textures/planetsurface/lovetexture.jpg'); // Load texture
+    sphereTexture.wrapS = THREE.RepeatWrapping; // Gentag texture horizontally
+    sphereTexture.wrapT = THREE.RepeatWrapping; // Gentag texture vertically
+
+    const sphereMat = new THREE.MeshStandardMaterial({
+        color: 0xffffff, 
+        emissive: 0xf5c277, // Glow farve
+        emissiveIntensity: .0, // glow intensity
+        map: sphereTexture, // tilføjer texture to vores material
+    
+    });
+
+    sphere = new THREE.Mesh(geometry, sphereMat); // Lav sphere med vores material
+    sphere.position.set(x, y, z); // Set positionen
+    scene.add(sphere); // Tilføj sphere til scenen
+}
+buildTSphere(0, 15, -80); // Bygger sphere med vores opdateret position
+
 
 
 // TÅLMODIGHEDS PLANET
-let sphere;
-function buildSphere(x, y, z) {
-    const geometry = new THREE.SphereGeometry(15, 32, 32); // Sphere geometrien
+let sphere01;
+function buildkSphere(x, y, z) {
+    const geometry = new THREE.SphereGeometry(10, 32, 32); // Sphere geometrien
     const sphereTexture = new THREE.TextureLoader().load('examples/textures/planetsurface/desserttexture.jpg'); // Load texture
     sphereTexture.wrapS = THREE.RepeatWrapping; // Gentag texture horizontally
     sphereTexture.wrapT = THREE.RepeatWrapping; // Gentag texture vertically
 
     const sphereMat = new THREE.MeshStandardMaterial({
         color: 0xffffff, 
+        emissive: 0xf7e1a3, // Glow farve
+        emissiveIntensity: .0, // glow intensity
         map: sphereTexture // tilføjer texture to vores material
     });
 
@@ -93,7 +139,47 @@ function buildSphere(x, y, z) {
     sphere.position.set(x, y, z); // Set positionen
     scene.add(sphere); // Tilføj sphere til scenen
 }
-buildSphere(0, 15, -80); // Bygger sphere med vores opdateret position
+buildkSphere(-37, 35, -180); // Bygger sphere med vores opdateret position
+
+
+// NYSGERRIGHEDS PLANET
+let sphere02;
+function buildnSphere(x, y, z) {
+    const geometry = new THREE.SphereGeometry(10, 32, 32); // Sphere geometrien
+    const sphereTexture = new THREE.TextureLoader().load('examples/textures/planetsurface/swamptexture.jpg'); // Load texture
+    sphereTexture.wrapS = THREE.RepeatWrapping; // Gentag texture horizontally
+    sphereTexture.wrapT = THREE.RepeatWrapping; // Gentag texture vertically
+
+    const sphereMat = new THREE.MeshStandardMaterial({
+        color: 0xffffff, 
+        emissive: 0x4dbe3f, // Glow farve
+        emissiveIntensity: .0, // glow intensity
+        map: sphereTexture // tilføjer texture to vores material
+    });
+
+    sphere = new THREE.Mesh(geometry, sphereMat); // Lav sphere med vores material
+    sphere.position.set(x, y, z); // Set positionen
+    scene.add(sphere); // Tilføj sphere til scenen
+}
+buildnSphere(37, 60, -180); // Bygger sphere med vores opdateret position
+
+
+
+
+// EFFEKTER 
+
+// BLLOOOM EFFEKKTTT ****
+const bloomPass = new UnrealBloomPass(new THREE.Vector2(vw, vh), 1.5, 2, 0.8);
+composer.addPass(bloomPass);
+
+console.log(bloomPass);
+
+
+
+
+
+
+
 
 
 
